83ba9e74cf2fa083dae60de830367a8f6328d0a6
ajay <rai.invisible.web@gmail.com>
2023-02-02 21:17:16 +0545

    First Laravel Project

--- routes/web.php
+++ routes/web.php
@@ -1,18 +1,13 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Backend\BackendController;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+Route::group(['namespace'=>'Backend','prefix'=>'company-admin'],function(){
+    Route::get('/',[BackendController::class,'index'])->name('dashboard');
 });
